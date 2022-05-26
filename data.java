d7f858c55b2fe6597df058ef570e8bd91f30c1bf
NicholasAdi24 <102377198+NicholasAdi24@users.noreply.github.com>
2022-05-27 00:01:24 +0700

    Create data.java

---
+++ data.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasakhir;
+
+  
+  
+class data {
+
+    /**
+     *
+     * @param nasi
+     * @param bm
+     * @param bp
+     * @param bb
+     * @param grm
+     * @param gl
+     * @param raym
+     * @param rspi
+     * @param ktg
+     * @param cb
+     * @param mgr
+     * @param wrtl
+     * @param dbw
+     * @param dgay
+     * @param dgspi
+     */
+    public void kurangistokmenu1(){
+        Tambahmenu dm = new Tambahmenu();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)-(dm.nasi1);
+        cs.stokbm = (cs.stokbm)-(dm.bwm1);
+        cs.stokbp = (cs.stokbp)-(dm.bwp1);
+        cs.stokbb = (cs.stokbb)-(dm.bwb1);
+        cs.stokgrm = (cs.stokgrm)-(dm.garem1);
+        cs.stokgl = (cs.stokgl)-(dm.gula1);
+        cs.stokryca = (cs.stokryca)-(dm.roycoA1);
+        cs.stokrycs = (cs.stokrycs)-(dm.roycoS1);
+        cs.stokktg = (cs.stokktg)-(dm.kentang1);
+        cs.stokcb = (cs.stokcb)-(dm.cabe1);
+        cs.stokmgr = (cs.stokmgr)-(dm.minyak1);
+        cs.stokwrtl = (cs.stokwrtl)-(dm.wortel1);
+        cs.stokdbw = (cs.stokdbw)-(dm.dnbw1);
+        cs.stokdgay = (cs.stokdgay)-(dm.dgay1);
+        cs.stokdgsp = (cs.stokdgsp)-(dm.dgsp1);
+       
+}
+    public void kurangistokmenu2(){
+        Tambahmenu2 dm = new Tambahmenu2();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)-(dm.nasi2);
+        cs.stokbm = (cs.stokbm)-(dm.bwm2);
+        cs.stokbp = (cs.stokbp)-(dm.bwp2);
+        cs.stokbb = (cs.stokbb)-(dm.bwb2);
+        cs.stokgrm = (cs.stokgrm)-(dm.garem2);
+        cs.stokgl = (cs.stokgl)-(dm.gula2);
+        cs.stokryca = (cs.stokryca)-(dm.roycoA2);
+        cs.stokrycs = (cs.stokrycs)-(dm.roycoS2);
+        cs.stokktg = (cs.stokktg)-(dm.kentang2);
+        cs.stokcb = (cs.stokcb)-(dm.cabe2);
+        cs.stokmgr = (cs.stokmgr)-(dm.minyak2);
+        cs.stokwrtl = (cs.stokwrtl)-(dm.wortel2);
+        cs.stokdbw = (cs.stokdbw)-(dm.dnbw2);
+        cs.stokdgay = (cs.stokdgay)-(dm.dgay2);
+        cs.stokdgsp = (cs.stokdgsp)-(dm.dgsp2);
+       
+}
+    public void kurangistokmenu3(){
+        Tambahmenu3 dm = new Tambahmenu3();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)-(dm.nasi3);
+        cs.stokbm = (cs.stokbm)-(dm.bwm3);
+        cs.stokbp = (cs.stokbp)-(dm.bwp3);
+        cs.stokbb = (cs.stokbb)-(dm.bwb3);
+        cs.stokgrm = (cs.stokgrm)-(dm.garem3);
+        cs.stokgl = (cs.stokgl)-(dm.gula3);
+        cs.stokryca = (cs.stokryca)-(dm.roycoA3);
+        cs.stokrycs = (cs.stokrycs)-(dm.roycoS3);
+        cs.stokktg = (cs.stokktg)-(dm.kentang3);
+        cs.stokcb = (cs.stokcb)-(dm.cabe3);
+        cs.stokmgr = (cs.stokmgr)-(dm.minyak3);
+        cs.stokwrtl = (cs.stokwrtl)-(dm.wortel3);
+        cs.stokdbw = (cs.stokdbw)-(dm.dnbw3);
+        cs.stokdgay = (cs.stokdgay)-(dm.dgay3);
+        cs.stokdgsp = (cs.stokdgsp)-(dm.dgsp3);
+       
+}
+    public void kurangistokmenu4(){
+        Tambahmenu4 dm = new Tambahmenu4();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)-(dm.nasi4);
+        cs.stokbm = (cs.stokbm)-(dm.bwm4);
+        cs.stokbp = (cs.stokbp)-(dm.bwp4);
+        cs.stokbb = (cs.stokbb)-(dm.bwb4);
+        cs.stokgrm = (cs.stokgrm)-(dm.garem4);
+        cs.stokgl = (cs.stokgl)-(dm.gula4);
+        cs.stokryca = (cs.stokryca)-(dm.roycoA4);
+        cs.stokrycs = (cs.stokrycs)-(dm.roycoS4);
+        cs.stokktg = (cs.stokktg)-(dm.kentang4);
+        cs.stokcb = (cs.stokcb)-(dm.cabe4);
+        cs.stokmgr = (cs.stokmgr)-(dm.minyak4);
+        cs.stokwrtl = (cs.stokwrtl)-(dm.wortel4);
+        cs.stokdbw = (cs.stokdbw)-(dm.dnbw4);
+        cs.stokdgay = (cs.stokdgay)-(dm.dgay4);
+        cs.stokdgsp = (cs.stokdgsp)-(dm.dgsp4);
+       
+}
+    public void kurangistokmenu5(){
+        Tambahmenu5 dm = new Tambahmenu5();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)-(dm.nasi5);
+        cs.stokbm = (cs.stokbm)-(dm.bwm5);
+        cs.stokbp = (cs.stokbp)-(dm.bwp5);
+        cs.stokbb = (cs.stokbb)-(dm.bwb5);
+        cs.stokgrm = (cs.stokgrm)-(dm.garem5);
+        cs.stokgl = (cs.stokgl)-(dm.gula5);
+        cs.stokryca = (cs.stokryca)-(dm.roycoA5);
+        cs.stokrycs = (cs.stokrycs)-(dm.roycoS5);
+        cs.stokktg = (cs.stokktg)-(dm.kentang5);
+        cs.stokcb = (cs.stokcb)-(dm.cabe5);
+        cs.stokmgr = (cs.stokmgr)-(dm.minyak5);
+        cs.stokwrtl = (cs.stokwrtl)-(dm.wortel5);
+        cs.stokdbw = (cs.stokdbw)-(dm.dnbw5);
+        cs.stokdgay = (cs.stokdgay)-(dm.dgay5);
+        cs.stokdgsp = (cs.stokdgsp)-(dm.dgsp5);
+       
+}
+    public void tambahistok (){
+        Tambahstok ts = new Tambahstok();
+        Cekstok cs = new Cekstok();
+        cs.stoknasi = (cs.stoknasi)+(ts.plusnasi);
+        cs.stokbm = (cs.stokbm)+(ts.plusbm);
+        cs.stokbp = (cs.stokbp)+(ts.plusbp);
+        cs.stokbb = (cs.stokbb)+(ts.plusbb);
+        cs.stokgrm = (cs.stokgrm)+(ts.plusgrm);
+        cs.stokgl = (cs.stokgl)+(ts.plusgl);
+        cs.stokryca = (cs.stokryca)+(ts.plusryca);
+        cs.stokrycs = (cs.stokrycs)+(ts.plusrycs);
+        cs.stokktg = (cs.stokktg)+(ts.plusktg);
+        cs.stokcb = (cs.stokcb)+(ts.pluscb);
+        cs.stokmgr = (cs.stokmgr)+(ts.plusmgr);
+        cs.stokwrtl = (cs.stokwrtl)+(ts.pluswrtl);
+        cs.stokdbw = (cs.stokdbw)+(ts.plusdbw);
+        cs.stokdgay = (cs.stokdgay)+(ts.plusdgay);
+        cs.stokdgsp = (cs.stokdgsp)+(ts.plusdgsp);
+}
+}
